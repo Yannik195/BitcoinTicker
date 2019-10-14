c97b0e39f97e8aa38e817792d070b4abbcf660ff
Yannik195 <yannisim@gmail.com>
2019-10-14 16:50:02 +0200

    Send actual HTML File

--- main.js
+++ main.js
@@ -5,14 +5,12 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", (req, res) => {
-  res.send("Hello Server!");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(3000, () => {
   console.log("Server Running");
-  console.log("Server Running");
-  console.log("Server Running");
-  console.log("Server Running");
-  
 });
