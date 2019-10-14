80ad4d796406d146cd470a6f163fbd7b219c02da
Yannik195 <yannisim@gmail.com>
2019-10-14 18:15:47 +0200

    Request API Data and parsing it into an JSON-Object

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
 
@@ -10,6 +11,19 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
+
+app.post("/" , (req, res) => {
+  console.log(req.body.crypto);
+
+  request("https://apiv2.bitcoinaverage.com/indices/global/ticker/" + req.body.crypto + req.body.fiat, (e, res, body) =>{
+    console.log("Body: " + body);
+    console.log(body);
+    var obj = JSON.parse(body);
+    console.log(obj.last);
+  });
+});
+
+
 
 app.listen(3000, () => {
   console.log("Server Running");
